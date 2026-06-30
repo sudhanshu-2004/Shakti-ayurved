2b09f0bb08829d04a90a03d59c621b65c525bf2e
sudhanshu kumar <sudhanshu.230434@andc.du.ac.in>
2026-07-01 02:15:47 +0530

    fix: restructure crm-api to use standard vercel api directory

--- crm-api/index.js
+++
@@ -1,7 +0,0 @@
-// Vercel Serverless Entry Point for Shakti Ayurved CRM API
-// This file wraps the Express app for Vercel's serverless runtime.
-// Vercel calls this as a Node.js function for all /crm-api/* requests.
-
-const app = require('./src/app');
-
-module.exports = app;
